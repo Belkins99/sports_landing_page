ac8b2a845dc2853502169af32a9f77f0466cc0c5
Mabel Aikins Acquah <acquahmabel60@gmail.com>
2024-06-14 13:11:10 +0000

    Final push

--- js/index.js
+++ js/index.js
@@ -1,5 +1,33 @@
-const btn = document.getElementById('bot')
-btn.onclick = function(){
+const bot = document.getElementById('bot')
+bot.onclick = function(){
     console.log('button has been clicked')
     alert('Match Starting in 15minutes')
 }
+
+const but = document.getElementById("but");
+but.onclick = function (){
+    const name = prompt('Enter your full name.');
+    document.getElementById('name').innerText = name;
+}
+
+
+const brt = document.getElementById("brt");
+brt.onclick = function (){
+    alert('Watch more exciting content')
+}
+
+
+const ret = document.getElementById("ret");
+ret.onclick = function (){
+    alert('Watch more live streams!!')
+}
+
+const view = document.getElementById("view");
+view.onclick = function (){
+    alert('View more tables')
+}
+
+
+
+
+
